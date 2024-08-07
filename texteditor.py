a8654ae6c22a459dfb92bd4191efeba0a50c831b
Garima Jaiswal <89124560+Garima-Jaiswal@users.noreply.github.com>
2024-08-07 09:36:49 +0530

    texteditor.py

--- texteditor.py
+++ texteditor.py
@@ -19,7 +19,7 @@ def save_file():
             messagebox.showinfo("Info","file saves successfully!")
 
 root= tk.Tk()
-root.title("Scriptify")
+root.title("WordWisp")
 root.geometry("800x600")
 
 menu= tk.Menu(root)
